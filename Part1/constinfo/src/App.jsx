2e36a22d1872b35d50d7c7508b511665593a9827
Matias Zurra <matiaszurra@hotmail.com>
2025-05-07 02:45:04 -0300

    Header y Total redefinidos

---
+++ Part1/constinfo/src/App.jsx
@@ -0,0 +1,39 @@
+
+const Header = (props) => {
+  return(
+    <h1>Header {props.course}</h1>
+  )
+}
+
+const Total = (props) => {
+ return(
+    <p>Total {props.total}</p>
+  )
+}
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  const total = (exercises1 + exercises2 + exercises3)
+  return (
+    <div>
+      <Header course = {course}/>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <Total total = {total}/>
+    </div>
+  )
+}
+
+export default App

3b0d7c7abf06886dcb25bee301c9f11e02da88fc
Matias Zurra <matiaszurra@hotmail.com>
2025-05-27 21:01:16 -0300

    Ejercicio 1.9 Terminado

--- Part1/unicafe/src/components/Statistics.jsx
+++ Part1/unicafe/src/components/Statistics.jsx
@@ -4,17 +4,22 @@ const Statistics = ({votes}) => {
     const getAll = () => votes.good + votes.neutral + votes.bad
     const getAverage = () => getAll()===0 ? 0 : (votes.good - votes.bad) / getAll()
     const getPositives = () => getAll()===0 ? 0 : (votes.good * 100) / getAll()
-  return (
-    <div>
-      <h2>statistics</h2>
-      <p>good {votes.good}</p>
-      <p>neutral {votes.neutral}</p>
-      <p>bad {votes.bad}</p>
-      <p>all {getAll()}</p>
-      <p>average {getAverage()}</p>
-      <p>positive {getPositives()} %</p>
-    </div>
-  )
+    if (getAll()===0){
+        return(
+            <p>No feedback given</p>
+        )
+    }else{
+    return(
+        <div>
+            <h2>statistics</h2>
+            <p>good {votes.good}</p>
+            <p>neutral {votes.neutral}</p>
+            <p>bad {votes.bad}</p>
+            <p>all {getAll()}</p>
+            <p>average {getAverage()}</p>
+            <p>positive {getPositives()} %</p>
+         </div>
+        )}
 }
 
 export default Statistics
